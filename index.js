0b7b391457d2f53306f6ebf02a81e9023f4c659b
rishabh <rgp02071993@gmail.com>
2021-02-22 18:22:05 +0530

    Added a post api

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ const express = require("express");
 
 require("dotenv").config()
 
+const bookRoute = require('./routes/boooks')
+
 
 const mongoose = require('mongoose')
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+// adding middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routes
+app.use('/api/books', bookRoute)
 
 // coonect to mongo db atlas
 mongoose.connect(process.env.MONGO_URL,
